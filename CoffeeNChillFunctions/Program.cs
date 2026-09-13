da1157447e067b5fc94b0087a74b6847e6533b28
st10275159 <st10275159@vcconnect.edu.za>
2026-09-13 21:42:27 +0200

    Fix isolated worker startup and register BlobServiceClient for staff-document functions

--- CoffeeNChillFunctions/Program.cs
+++ CoffeeNChillFunctions/Program.cs
@@ -1,7 +1,23 @@
+using Azure.Storage.Blobs;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services =>
+    {
+        // Used by the staff-document functions (Upload/List/Download).
+        // AzureWebJobsStorage is set in local.settings.json for `func start`,
+        // and passed as a Docker environment variable for the standalone
+        // Functions container (see README).
+        services.AddSingleton(_ =>
+        {
+            string connectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage")
+                ?? "UseDevelopmentStorage=true";
+
+            return new BlobServiceClient(connectionString);
+        });
+    })
     .Build();
 
 host.Run();
